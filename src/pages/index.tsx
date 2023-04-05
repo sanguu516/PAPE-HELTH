ee915332a4f75d0d114a2382c043c2c2321e78c8
sanguu516 <sanguu516@naver.com>
2023-04-05 11:38:38 +0900

    tailwindcss추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,5 +6,12 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="border-double border-4 border-sky-500">
+        Save Changes
+      </button>
+    </>
+  );
 }
