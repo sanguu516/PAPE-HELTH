d9cf7845d8ebc712f752e4251058e1cfd43f56c1
sanguu516 <sanguu516@naver.com>
2023-04-07 11:06:18 +0900

    슬라이드 기능(반응형 조금 추가)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,42 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Header from "./header";
-const inter = Inter({ subsets: ["latin"] });
+import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useRef, useState } from "react";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-export default function Home() {
+export default function index() {
   return (
     <div className="">
       <Header />
       <section className=" bg-main-color w-full h-full flex justify-center">
-        <div className="w-[300px] h-[320px] mx-[20px] mb-[20px]	bg-white drop-shadow-xl"></div>
-        <div className="flex flex-col w-[880px] h-[320px]">
-          <div className="w-full h-full mb-[20px] border-solid border-2"></div>
-          <div className="w-full h-full  border-solid border-2"></div>
+        <div className="w-[300px] h-[320px] mx-[20px] mb-[20px]	bg-white drop-shadow-xl "></div>
+        <div className="flex flex-col lg:w-[880px]  w-[220px] h-[320px]">
+          <div className="w-full h-full mb-[20px]  border-solid border-2 ">
+            <Swiper
+              cssMode={true}
+              navigation={true}
+              pagination={true}
+              mousewheel={true}
+              keyboard={true}
+              modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+              className="mySwiper text-xl"
+            >
+              <SwiperSlide>슬라이드쇼 1</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 2</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 3</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 4</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 5</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 6</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 7</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 8</SwiperSlide>
+              <SwiperSlide>슬라이드쇼 9</SwiperSlide>
+            </Swiper>
+          </div>
+          <div className="w-full h-full border-solid border-2 bg-white"></div>
         </div>
       </section>
     </div>
